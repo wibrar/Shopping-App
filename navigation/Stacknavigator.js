b407c2713e4c4cdef0fbb6416a4f72e353416d67
wasif <wibrar@mun.ca>
2024-01-18 07:20:14 -0330

    login register and Bottom tab added in stack navigator

---
+++ navigation/Stacknavigator.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Loginscreen from '../screens/Loginscreen';
+import Registerscreen from '../screens/Registerscreen';
+import Search from '../screens/Search';
+import Product from '../screens/Product';
+import Catagoriesscreen from '../screens/Catagoriesscreen';
+import BottomTab from './BottomTab';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='BottmTab'>
+        <Stack.Screen name="Login" component={Loginscreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Registerscreen} options={{ headerShown: false }} />
+        <Stack.Screen name="BottmTab" component={BottomTab} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default StackNavigator;
